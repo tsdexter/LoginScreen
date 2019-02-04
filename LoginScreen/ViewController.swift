0e19edd1897f75f602a378530b0e03fd27d50148
Thomas <tsdexter@gmail.com>
2019-02-04 13:19:38 -0500

    end of video 2

--- LoginScreen/ViewController.swift
+++ LoginScreen/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // instantiate fields/labels
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
+    /**
+     * Handle the 'Log In' click event
+     */
+    @IBAction func onLoginClick(_ sender: Any) {
+        // get field values
+        let username = usernameField.text
+        let password = passwordField.text
+        
+        // check login info
+        if (username == "test" && password == "test") {
+            errorLabel.isHidden = true
+        } else {
+            errorLabel.isHidden = false
+        }
+    }
 }
 
